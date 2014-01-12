79363d9b0230dcc7ea2630f9712bf761e7ea3438
Rashi Abramson <roshkins@gmail.com>
2014-01-12 14:07:38 -0800

    Added quit command

--- chat_server.rb
+++ chat_server.rb
@@ -4,26 +4,45 @@ require 'thread'
 class ChatRunner
 	class ChatServer < WEBrick::GenericServer
 
+		def proccess_command message, sock
+
+			command = message.split(/ /,2)
+			command[0] = command.first.downcase.strip
+
+			puts "Command logged: #{command.join " "}"
+
+			if ["quit"].include? command.first
+				self.send *command, sock
+			else
+				sock.puts "Not a valid command."
+			end
+		end
+
+		def quit *args, sock
+				sock.puts "BYE"
+				Thread.kill(Thread.current)
+		end
+
 		def run(sock)
 			sock.puts "Hello world!"
 			@threads ||= []
-			mutex = Mutex.new
-			@message = nil
+			message = nil
 			@threads << Thread.new(sock) do |sock|
 				Thread.abort_on_exception = true
 				loop do
 					Thread.stop
-					sock.puts @message[:message]
+					sock.puts message[:message]
 				end
 			end
 			loop do
 				sock.print "Enter a message to tell the world: "
-				mutex.synchronize do
-					@message = {:time => Time.new, :message=> sock.gets}
-				end
-				@threads.each do |thread|
-					p thread.status
-				 	thread.run if thread.status == "sleep"
+				message = {:time => Time.new, :message => sock.gets}
+				if message[:message][0] == "/"
+					self.proccess_command message[:message][1..-1], sock
+				else
+					@threads.each do |thread|
+					 	thread.run if thread.status == "sleep"
+					end
 				end
 			end
 		end
